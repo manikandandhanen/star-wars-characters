5ec4b9cd15aef33fb2d088995f6ae598486381d6
Manikandan D <108661573+manikandandhanen@users.noreply.github.com>
2024-07-03 18:41:45 +0530

    Manikandan/features/init (#3)
    * initial commit
    
    * init
    
    * list view updated
    
    * list view updated
    
    ---------
    
    Co-authored-by: Manikandan Dhanen <manikandan.dhanen@mastec.com>

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -11,6 +11,13 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-character-list',
   standalone: true,
@@ -24,36 +31,150 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatPaginatorModule,
     MatSortModule,
     MatTableModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
     MatProgressSpinnerModule,
     CharacterFilterComponent,
+    FormsModule,
   ],
 })
 export class CharacterListComponent implements OnInit {
-  displayedColumns: string[] = ['avatar', 'name', 'details'];
+  displayedColumns: string[] = [
+    'sno',
+    'avatar',
+    'name',
+    'species',
+    'birth_year',
+    'details',
+  ];
   dataSource!: MatTableDataSource<any>;
+  originalData: any[] = [];
+
+  minBirthYear: string = '';
+  maxBirthYear: string = '';
+  matchingCharactersCount: number = 0;
+
+  selectedMovies: string[] = [];
+  selectedSpecies: string[] = [];
+  selectedBirthYears: string[] = [];
+  moviesList: string[] = [];
+  speciesList: string[] = [];
+  birthYears: any[] = [];
+  filmMap: Map<string, string> = new Map();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private characterService: CharacterService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
     this.characterService.getCharacters().subscribe((data) => {
+      this.originalData = data.results;
       this.dataSource = new MatTableDataSource(data.results);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
+      this.mapSpeciesNames();
+    });
+    this.getSpeciesList();
+    this.getMoviesList();
+  }
+
+  getSpeciesList(): void {
+    this.characterService.getSpecies().subscribe((data) => {
+      this.speciesList = data.results.map(
+        (species: { name: any }) => species.name
+      );
     });
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  getMoviesList(): void {
+    this.characterService.getMovies().subscribe((data) => {
+      this.moviesList = data.results.map((movie: any) => movie.title);
+      data.results.forEach((movie: any) => {
+        this.filmMap.set(movie.url, movie.title);
+      });
+    });
+  }
+
+  mapSpeciesNames() {
+    const speciesRequests = this.originalData.map((character: any) => {
+      if (character.species.length > 0) {
+        return this.http.get(character.species[0]).toPromise();
+      } else {
+        return Promise.resolve({ name: 'Unknown' });
+      }
+    });
+
+    Promise.all(speciesRequests)
+      .then((speciesData: any[]) => {
+        this.originalData.forEach((character: any, index: number) => {
+          character.species = speciesData[index].name;
+        });
+        this.applyFilter();
+      })
+      .catch((error) => {
+        console.error('Error fetching species data:', error);
+      });
+  }
+
+  applyFilter() {
+    const filteredData = this.originalData.filter((character: any) => {
+      const movieNames = character.films.map((filmUrl: string) =>
+        this.filmMap.get(filmUrl)
+      );
+
+      const movieFilter =
+        this.selectedMovies.length === 0 ||
+        this.selectedMovies.every((movie) => movieNames.includes(movie));
+
+      const speciesFilter =
+        this.selectedSpecies.length === 0 ||
+        this.selectedSpecies.includes(character.species);
+
+      const birthYearFilter = this.isWithinBirthYearRange(character.birth_year);
+
+      return movieFilter && speciesFilter && birthYearFilter;
+    });
+
+    this.dataSource.data = filteredData;
+
+    this.matchingCharactersCount = filteredData.length;
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
+    }
+  }
+
+  isWithinBirthYearRange(birthYear: string): boolean {
+    if (!this.minBirthYear && !this.maxBirthYear) {
+      return true;
+    }
+
+    const year = this.convertBirthYearToNumber(birthYear);
+    const minYear = this.maxBirthYear
+      ? this.convertBirthYearToNumber(this.maxBirthYear)
+      : -Infinity;
+    const maxYear = this.minBirthYear
+      ? this.convertBirthYearToNumber(this.minBirthYear)
+      : Infinity;
+
+    return year >= minYear && year <= maxYear;
+  }
+
+  convertBirthYearToNumber(birthYear: string): number {
+    birthYear = birthYear.toUpperCase();
+    if (birthYear.includes('BBY')) {
+      return -parseFloat(birthYear.replace('BBY', ''));
+    } else if (birthYear.includes('ABY')) {
+      return parseFloat(birthYear.replace('ABY', ''));
+    } else {
+      return 0;
     }
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,19 +4,10 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://swapi.dev/api/people',
+  baseUrl: 'https://swapi.dev/api',
   useHash: false,
 
   //Backend API's
 
-  LOGIN_URL: 'https://swapi.dev/api/people',
+  API_URL: 'https://swapi.dev/api',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
