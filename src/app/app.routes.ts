4c189edb6671c1c99689af9a488ed64fa98931af
Manikandan D <108661573+manikandandhanen@users.noreply.github.com>
2024-07-03 22:22:57 +0530

    Manikandan/features/init (#4)
    * initial commit
    
    * init
    
    * list view updated
    
    * list view updated
    
    * list view updated
    
    ---------
    
    Co-authored-by: Manikandan Dhanen <manikandan.dhanen@mastec.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { CharacterDetailsComponent } from './components/character-details/character-details.component';
 import { CharacterListComponent } from './components/character-list/character-list.component';
+import { SidenavMenuComponent } from './components/sidenav-menu/sidenav-menu.component';
 
 export const routes: Routes = [
-  { path: '', component: CharacterListComponent },
-  { path: 'characters/:id', component: CharacterDetailsComponent },
+  {
+    path: '',
+    component: SidenavMenuComponent,
+    children: [
+      { path: '', component: CharacterListComponent },
+      { path: 'characters/:id', component: CharacterDetailsComponent },
+    ],
+  },
+  { path: '**', redirectTo: '' },
 ];
